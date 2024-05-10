9cc75dea3ab56e75e7fd8ef7f169d76debb58fe0
deepak-gameti <159270881+deepak-gameti@users.noreply.github.com>
2024-05-10 09:22:37 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { Server } from 'lucide-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
